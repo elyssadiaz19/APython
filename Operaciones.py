de3bb6d6c1f6084e58df0f5ca563c93fd19b92a0
Elizabeth Díaz <87160597+elyssadiaz19@users.noreply.github.com>
2021-07-10 11:23:16 -0500

    Add files via upload

---
+++ Operaciones.py
@@ -0,0 +1,6 @@
+numero_uno = int(input())
+numero_dos = int(input())
+resultadoSuma = numero_uno + numero_dos
+resultadoResta = numero_uno -numero_dos
+resultadoMultiplicacion = numero_uno * numero_dos
+print("Total suma: ",resultadoSuma,"\nTotal Resta: ",resultadoResta,"\nTotal Multiplicacion: ",resultadoMultiplicacion)
